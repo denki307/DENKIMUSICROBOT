d7ba0b3fbe7950a39ac7e158b3b01ac86f900ce8
denki307 <200189507+denki307@users.noreply.github.com>
2026-01-18 09:52:42 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from AnonXMusic import app
+from AviaxMusic import app
 
 
 def start_panel(_):
@@ -10,14 +10,14 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
     return buttons
 
 
 def private_panel(_):
-    buttons =
+    buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
@@ -26,11 +26,17 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text="ð– ð–¯ð–¨ ð–¢ð—ˆð—‡ð—Œð—ˆð—…ð–¾",
+                callback_data="api_console"
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ]
-    
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),       
+        ],
+    ]
     return buttons
